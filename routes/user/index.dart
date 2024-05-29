9776046b07740e5edb4fed512f6631f0170ad5a9
KookDohyun <rnrtmdtn1213@outlook.com>
2024-05-29 12:45:36 +0900

    update

--- routes/user/index.dart
+++ routes/user/index.dart
@@ -1,5 +1,17 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'This is a new route!');
+  return switch (context.request.method) {
+    HttpMethod.get=> _getUsers(),
+    _ => Response(body: 'This is default')
+  };
+}
+
+Response _getUsers() {
+  return Response.json(
+    body: [
+      {'name': 'Dohyun', 'lastname': 'Kook', 'age': 29},
+      {'name': 'Bunjun', 'lastname': 'Park', 'age': 25},
+    ],
+  );
 }
